50b45b84b46ed3b8a2dfa670debee80bdb86c345
pkosmogl <polidamas.kosmoglou@cern.ch>
2022-04-15 14:45:11 +0200

    Simplifying the procedure

--- HTCondorSubmission_for_BMTFvalidation/cloneFactory.sh
+++ HTCondorSubmission_for_BMTFvalidation/cloneFactory.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 
-### Only dataset and gTag variables need to be set in the original python configuration file ###
-
 if [ "$#" -ne 9 ]
 then
    echo
-   printf "=%.0s" {1..73}; printf "\n"
-   echo "Usage:   $0  <python file> <Nevents> <lumi start> <lumi step> <lumi stop> <run number> <submission file> <job flavor> <executable file>"
-   printf "=%.0s" {1..73}; printf "\n"
+   printf "=%.0s" {1..125}; printf "\n"
+   echo "Usage:   $0  <python file> <Nevents> <dataset> <run #> <gTag> <lumi start> <lumi step> <lumi stop> <directory>"
+   printf "=%.0s" {1..125}; printf "\n"
    echo "python file         ---> the cfg file to be cloned"
    echo "Nevents             ---> number of events"
+   echo "dataset             ---> the dataset to run upon"
+   echo "run #               ---> run number of interest"
+   echo "gTag                ---> global tag"
    echo "lumi start          ---> start from Lumi Section" 
    echo "lumi step           ---> step between Lumi Section start/stop"
    echo "lumi stop           ---> stop to Lumi Section"
-   echo "run number          ---> run number of interest"
-   echo "submission file     ---> submission file used in condor_submit"
-   echo "job flavor          ---> choose one from the following list:"
-   echo "                         - espresso     (20 min)"  
-   echo "                         - microcentury (1 hour)"
-   echo "                         - longlunch    (2 hour)"
-   echo "                         - workday      (8 hour)"
-   echo "                         - tomorrow     (1 day)"
-   echo "                         - testmatch    (3 days)"
-   echo "                         - nextweek     (1 week)"
-   echo "executable file     ---> executable file used for cmsRun"
-   printf "=%.0s" {1..73}; printf "\n"
+   echo "directory           ---> directory to be created to store the python clones"
+   printf "=%.0s" {1..125}; printf "\n"
    echo
    exit 1
 fi
@@ -32,55 +23,62 @@ fi
 # Take aruments
 pyfile=$1                                    # Python file name
 pyNoDot=$(echo "$pyfile" | cut -f 1 -d '.')  # Remove the .py extension from python file name
+if ! [[ "$pyfile" =~ ^.*\.py$ ]]; then
+   echo "===> Error : Invalid python file!"
+   exit 1
+fi
 
 Nevents=$2  # Number of events
-Lstart=$3   # Lumi Section start, min range
-step=$4     # Lumi Section step
-Lstop=$5    # Lumi Section stop, max range
-runNum=$6   # Run number
+runNum=$4   # Run number
+Lstart=$6   # Lumi Section start, min range
+step=$7     # Lumi Section step
+Lstop=$8    # Lumi Section stop, max range
+if ! [[ "$Nevents" =~ ^[0-9]+$ ]]; then
+   echo "===> Error : Invalid number of events!"
+   exit 1
+elif ! [[ "$runNum" =~ ^[0-9]+$ ]]; then
+   echo "===> Error : Invalid run number!"
+   exit 1
+elif ! [[ "$Lstart" =~ ^[0-9]+$ ]]; then
+   echo "===> Error : Invalid LS to begin!"
+   exit 1
+elif ! [[ "$step" =~ ^[0-9]+$ ]]; then
+   echo "===> Error : Invalid LS step!"
+   exit 1
+elif ! [[ "$Lstop" =~ ^[0-9]+$ ]]; then
+   echo "===> Error : Invalid LS to stop!"
+   exit 1
+fi
+
+dataSet=$3  # Dataset to run upon
+globTag=$5  # Global Tag
+direct=$9   # Create a new directory
+if [ ! -d $direct ]; then mkdir $direct; fi
+cd $direct
 
 LSintervals=($(seq $Lstart $step $Lstop))  # Lumi intervals
 Nclones=$((${#LSintervals[@]} - 1))        # Number of python file clones to be created
 
-subfile=$7                                     # submission file used in condor_submit
-subNoDot=$(echo "$subfile" | cut -f 1 -d '.')  # Remove the .sub extension from submission file name
-
-flav=$8  # Job flavor
-
-exefile=$9                                     # executable file used for cmsRun
-exeNoDot=$(echo "$exefile" | cut -f 1 -d '.')  # Remove the .sh extension from submission file name
-
 # Create clones of python, submission and executable files
 echo "Creating $Nclones python file clones..."
-echo "Creating $Nclones submission file clones..."
-echo "Creating $Nclones executable file clones..."
-for ((i = 0 ; i < $Nclones ; i++)) 
+echo "Path: $PWD"
+for ((i = 0 ; i < $Nclones ; i++))
 do
    j=$(($i + 1))
    ext=_"$runNum"RunNumber_"$Nevents"Events_"${LSintervals[$i]}"LS_"$((${LSintervals[$j]} - 1))"LS
    newpy="$pyNoDot""$ext".py
-   newsub="$subNoDot""$ext".sub
-   newexe="$exeNoDot""$ext".sh
 
-   sed 's@events = 100@events = '$Nevents'@' $pyfile > ./Run_348776_validationDate_06_April_2022/$newpy
-   sed -i 's@run = "[0-9]*"@run = "'$runNum'"@' ./Run_348776_validationDate_06_April_2022/$newpy
-   sed -i 's@lumiBegin = "[0-9]*"@lumiBegin = "'${LSintervals[$i]}'"@' ./Run_348776_validationDate_06_April_2022/$newpy
-   sed -i 's@lumiEnd = "[0-9]*"@lumiEnd = "'$((${LSintervals[$j]} - 1))'"@' ./Run_348776_validationDate_06_April_2022/$newpy
-
-   sed 's@exe.sh@'$newexe'@' $subfile > ./Run_348776_validationDate_06_April_2022/$newsub
-   sed -i 's@output\/output.out@output\/output'$ext'.out@' ./Run_348776_validationDate_06_April_2022/$newsub
-   sed -i 's@error\/error.err@error\/error'$ext'.err@' ./Run_348776_validationDate_06_April_2022/$newsub
-   sed -i 's@log\/log.log@log\/log'$ext'.log@' ./Run_348776_validationDate_06_April_2022/$newsub
-   sed -i 's@flavour@'$flav'@' ./Run_348776_validationDate_06_April_2022/$newsub
-
-   sed 's@afspath@'$PWD'\/Run_348776_validationDate_06_April_2022@' $exefile > ./Run_348776_validationDate_06_April_2022/$newexe
-   sed -i 's@cfg.py@'$newpy'@' ./Run_348776_validationDate_06_April_2022/$newexe 
+   sed 's@events = [0-9]*@events = '$Nevents'@' ../$pyfile > $newpy
+   sed -i 's@dataset = ".*"@dataset = "'$dataSet'"@' $newpy
+   sed -i 's@run = "[0-9]*"@run = "'$runNum'"@' $newpy
+   sed -i 's@gTag = ".*"@gTag = "'$globTag'"@' $newpy
+   sed -i 's@lumiBegin = "[0-9]*"@lumiBegin = "'${LSintervals[$i]}'"@' $newpy
+   sed -i 's@lumiEnd = "[0-9]*"@lumiEnd = "'$((${LSintervals[$j]} - 1))'"@' $newpy
 done
 echo "Done"
 
 # Create directories error - log - output
-cd ./Run_348776_validationDate_06_April_2022
-
 if [ ! -d error ]; then mkdir error; fi
 if [ ! -d log ]; then mkdir log; fi
 if [ ! -d output ]; then mkdir output; fi
+exit 0
--- HTCondorSubmission_for_BMTFvalidation/exe.sh
+++ HTCondorSubmission_for_BMTFvalidation/exe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-cd afspath
+cd $4
 eval `scramv1 runtime -sh`        # this is the cmsenv
-export X509_USER_PROXY=$1         # set path to proxy file
+export X509_USER_PROXY=$3         # set path to proxy file
 export HOME=$2                    # set path to home directory (otherwise ~/.dasmaps directory cant be accessed)
 
-cmsRun cfg.py
+cmsRun $1
